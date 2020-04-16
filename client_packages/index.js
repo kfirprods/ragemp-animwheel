8bf12fb9b317a94020790f8b49361446c484513e
Kfir <kfirprods@gmail.com>
2020-04-16 22:25:40 +0300

    Removed unnecessary files and npm packages

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,7 +1,5 @@
-require('rage-rpc/index.js');
-
 require('freeroam/index.js');
 
+// rage-rpc is required by animwheel
+require('rage-rpc/index.js');
 require('animwheel/index.js');
-
-require('greenscreen/index.js');
